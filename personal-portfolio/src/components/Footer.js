d42ed17ea6de9a9939e8d3bf79c89eb1c8d0afac
Jay Chong <jaychong1203@gmail.com>
2024-03-19 15:00:10 -0700

    replaced old github links

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -14,7 +14,7 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://github.com/Kizum1" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/K1zum1" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://www.linkedin.com/in/jaeyoon-chong/" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Icon" /></a>
               <a href="https://leetcode.com/kizum1/" target="_blank" rel="noopener noreferrer"><SiLeetcode size={20} color="#FFF" /></a>
             </div>
