e07627e69fd02694730b70fe80416ab486c862cf
Jay Chong <jaychong1203@gmail.com>
2024-04-02 18:46:39 -0700

    reconfigure

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Projects } from "./components/Projects";
-import { Footer } from "./components/Footer";
+
 import { EduTimeline } from './components/EduTimeline';
 import { Techstack } from './components/Tech';
 import { Toolstack } from './components/Tool';
@@ -13,14 +13,14 @@ import { Toolstack } from './components/Tool';
 function App() {
   return (
     <div className="App">
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
       <NavBar />
       <Banner />
+      </div>
       <EduTimeline />
       <Projects />
       <Techstack />
       <Toolstack />
-      <Footer />
-      
     </div>
   );
 }
