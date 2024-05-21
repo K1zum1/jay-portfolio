223d3708905e716f37f5bee5bb5dbbe2fbcc573c
Jay <jaychong1203@gmail.com>
2024-05-21 11:22:14 -0400

    updating projects

--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -6,53 +6,51 @@ import projImg3 from "../assets/img/project-img3.png";
 import projImg4 from "../assets/img/project-img4.png";
 import projImg5 from "../assets/img/project-img5.png";
 import projImg6 from "../assets/img/project-img6.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
-import colorSharp1 from "../assets/img/color-sharp.png";
 import 'animate.css';
 
 export const Projects = () => {
   const projects = [
     {
-      title: "Viking Hacks Website",
-      description: "Website for Viking Hacks 2023",
-      Tags: "HTML, CSS, Javascript",
-      imgUrl: projImg1,
-      githubUrl: "https://github.com/Kizum1/vikinghacks"
+      title: "Wind Density Modeler",
+      description: "Stellar Wind Density Modeler for protoplanetary disks. Used for observing early star formation.",
+      Tags: "Python, NumPy, SciPy, Matplotlib",
+      imgUrl: projImg3,
+      githubUrl: "https://github.com/K1zum1/Disc-Wind-Density-Program"
     },
     {
       title: "Notion Reminder Bot",
-      description: "Discord bot that sends reminders from Notion to Discord",
+      description: "Discord bot that sends reminders from Notion to Discord. Uses Notion API to fetch data. ",
       Tags: "Discord JS, Javascript, Notion API",
       imgUrl: projImg5,
       githubUrl: "https://github.com/pluffpenguin/Notion-Deadline-Reminder-Bot"
     },
     {
       title: "VReader",
-      description: "VR book reader for the Oculus Quest 2",
+      description: "VR book reader for the Oculus Quest 2. Launched using Roblox Games",
       Tags: "Lua, Roblox Studios",
       imgUrl: projImg2,
       githubUrl: "https://github.com/Kizum1/VReader"
     },
     {
+      title: "Viking Hacks Website",
+      description: "Website for Viking Hacks 2023. ",
+      Tags: "HTML, CSS, Javascript",
+      imgUrl: projImg1,
+      githubUrl: "https://github.com/Kizum1/vikinghacks"
+    },
+    {
       title: "Worldview Wanderer",
-      description: "Destination Viewer with local cuisine and music.",
+      description: "Basically a powerpoint built from scratch with some extra features like music and animations. ",
       Tags: "Python, Pygame",
       imgUrl: projImg4,
       githubUrl: "https://github.com/TeachMeTW/WorldView-Wanderer"
     },
     {
-      title: "Wind Density Modeler",
-      description: "Simulates stellar accretion disk winds, calculating the wind density",
-      Tags: "Python, Jupyter Notebook",
-      imgUrl: projImg3,
-      githubUrl: "https://github.com/K1zum1/Disc-Wind-Density-Program"
-    },
-    {
-      title: "GDSC Website",
-      description: "Website for DVC Developers",
-      Tags: "HTML, CSS, Javascript",
+      title: "School Projects",
+      description: "Various school projects from my time at the Diablo Valley College.",
+      Tags: "Python, C++, Assembly",
       imgUrl: projImg6,
-      githubUrl: "https://github.com/Kizum1/gdscwebsite"
+      githubUrl: "https://github.com/K1zum1/school-projects"
     },
   ];
 
@@ -63,7 +61,6 @@ export const Projects = () => {
           <Col size={12}>
             <div className="animate__animated animate__fadeIn">
               <h2>Projects</h2>
-              <p>Here are some projects I have worked on</p>
                 <Row>
                 {projects.map((project, index) => (
                   <ProjectCard key={index} {...project} />
